b491f674cc50b41f2350b3b9aa15ef804f007fbd
Robert Collins <collinsrj@hotmail.com>
2025-03-17 09:01:54 +0000

    first commit

---
+++ src/main/java/com/collinsrj/demotaskservice/DemotaskserviceApplication.java
@@ -0,0 +1,13 @@
+package com.collinsrj.demotaskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotaskserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotaskserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collinsrj/demotaskservice/DemotaskserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collinsrj.demotaskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotaskserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
